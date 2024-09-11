8b14a7864db76a0b8d2969a91907b73848f0b6b0
sarathrajendran552 <sarath.tg@gmail.com>
2024-09-11 23:10:26 +0530

    Auto-sync on Wednesday 11 September 2024 11:10:26 PM IST

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,63 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.enable_scroll_bar = true
+
+-- This tells wezterm to look first for fonts in the directory named
+-- `fonts` that is found alongside your `wezterm.lua` file.
+-- As this option is an array, you may list multiple locations if
+-- you wish.
+config.font_dirs = { 'fonts' }
+
 
 -- This is where you actually apply your config choices
 
+config.keys = {
+  -- This will create a new split and run your default program inside it
+  {
+    key = 'h',
+    mods = 'ALT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  -- This will create a new split and run your default program inside it
+  {
+    key = 'v',
+    mods = 'ALT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'LeftArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'RightArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Up',
+  },
+  {
+    key = 'DownArrow',
+    mods = 'ALT',
+    action = act.ActivatePaneDirection 'Down',
+  },
+	-- ChangeWindowKeys
+  { key = 'RightArrow', mods = 'CTRL', action = act.ActivateTabRelative(1) },
+  { key = 'LeftArrow', mods = 'CTRL', action = act.ActivateTabRelative(-1) },
+
+}
+
+
+
 -- For example, changing the color scheme:
 config.color_scheme = 'OceanicMaterial'
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'MesloLGS NF'
 config.window_background_opacity = 0.98
 config.text_background_opacity = 1
 
